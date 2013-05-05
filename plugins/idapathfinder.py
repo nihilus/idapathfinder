11979f74d655ca4d9c7c4b154a466f869eda7a48
heffnercj <heffnercj@9f6eff26-a04f-d2ae-6ea1-cf895e0f8755>
2013-05-05 01:52:26 +0000

    Replaced text input box with modal chooser window.

--- plugins/idapathfinder.py
+++ plugins/idapathfinder.py
@@ -117,6 +117,36 @@ class PathFinderGraph(idaapi.GraphViewer):
 		self.Refresh()
 
 
+class FunctionChooser(idaapi.Choose2):
+
+	def __init__(self):
+		idaapi.Choose2.__init__(self, "Choose a function", [["Function name", 50 | idaapi.Choose2.CHCOL_PLAIN]])
+		self.icon = 41
+		self.PopulateItems()
+
+	def PopulateItems(self):
+		self.items = [[idc.Name(f)] for f in idautils.Functions()]
+
+	def OnGetLine(self, n):
+		return self.items[n]
+
+	def OnGetSize(self):
+		return len(self.items)
+
+	def OnClose(self):
+		'''
+		Although we don't use it, this MUST be defined or the chooser will never even be displayed by IDA.
+		'''
+		pass
+
+	def GetUserInput(self):
+		n = self.Show(modal=True)
+		if n > -1:
+			return self.items[n][0]
+		else:
+			return None
+
+
 class idapathfinder_t(idaapi.plugin_t):
 
 	flags = 0
@@ -126,7 +156,7 @@ class idapathfinder_t(idaapi.plugin_t):
 	wanted_hotkey = ''
 
 	def init(self):
-		self.menu_context = idaapi.add_menu_item("View/Graphs/", "Find all paths to...", "Alt-5", 0, self.run, (None,))
+		self.menu_context = idaapi.add_menu_item("View/Graphs/", "Find paths from current function", "Alt-5", 0, self.run, (None,))
 		return idaapi.PLUGIN_KEEP
 
 	def term(self):
@@ -134,11 +164,12 @@ class idapathfinder_t(idaapi.plugin_t):
 		return None
 	
 	def run(self, arg):
-		ea = ScreenEA()
-		source = GetFunctionName(ea)
+		source = GetFunctionName(ScreenEA())
 
 		if source:
-			target = AskIdent(source, 'Find paths to')
+			fc = FunctionChooser()
+			target = fc.GetUserInput()
+			#target = AskIdent(source, 'Find paths to')
 			if target:
 				pf = pathfinder.PathFinder(target)
 				results = pf.paths_from(source)
@@ -152,4 +183,3 @@ class idapathfinder_t(idaapi.plugin_t):
 
 def PLUGIN_ENTRY():
 	return idapathfinder_t()
-
