755286f0f7bcbd4b0c340612b291a18f41be630c
heffnercj <heffnercj@9f6eff26-a04f-d2ae-6ea1-cf895e0f8755>
2013-05-05 14:04:53 +0000

    Added ability to plot one-to-many graphs.

--- plugins/idapathfinder.py
+++ plugins/idapathfinder.py
@@ -6,8 +6,7 @@ import pathfinder
 class FunctionChooser(idaapi.Choose2):
 
 	def __init__(self):
-		idaapi.Choose2.__init__(self, "Choose a function", [["Function name", 50 | idaapi.Choose2.CHCOL_PLAIN]])
-		self.icon = 41
+		idaapi.Choose2.__init__(self, "Choose a function", [["Function name", 50 | idaapi.Choose2.CHCOL_PLAIN]], icon=41)
 		self.PopulateItems()
 
 	def PopulateItems(self):
@@ -42,15 +41,36 @@ class idapathfinder_t(idaapi.plugin_t):
 	wanted_hotkey = ''
 
 	def init(self):
-		self.menu_context = idaapi.add_menu_item("View/Graphs/", "Find paths from current function", "Alt-5", 0, self.run, (None,))
+		ui_path = "View/Graphs/"
+		self.menu_contexts = []
+
+		self.menu_contexts.append(idaapi.add_menu_item(ui_path, 
+								"Find multiple paths from current function", 
+								"Alt-6", 
+								0, 
+								self.FindPathsToMany, 
+								(None,)))
+		self.menu_contexts.append(idaapi.add_menu_item(ui_path, 
+								"Find single path from current function", 
+								"Alt-5", 
+								0, 
+								self.FindPathsToSingle, 
+								(None,)))
 		return idaapi.PLUGIN_KEEP
 
 	def term(self):
-		idaapi.del_menu_item(self.menu_context)
+		for context in self.menu_contexts:
+			idaapi.del_menu_item(context)
 		return None
 	
 	def run(self, arg):
-		source = GetFunctionName(ScreenEA())
+		self.FindPathsToSingle()
+
+	def _current_function(self):
+		return GetFunctionName(ScreenEA())
+
+	def FindPathsToSingle(self, arg):
+		source = self._current_function()
 
 		if source:
 			fc = FunctionChooser()
@@ -63,8 +83,30 @@ class idapathfinder_t(idaapi.plugin_t):
 				g = pathfinder.PathFinderGraph(results, "Call graph from " + source + " to " + target)
 				g.Show()
 				del g
-		else:
-			print "ERROR: Address 0x%X is not part of a defined function." % ea
+
+	def FindPathsToMany(self, arg):
+		targets = []
+		results = []
+		source = self._current_function()
+
+		if source:
+			fc = FunctionChooser()
+			while True:
+				target = fc.GetUserInput()
+				if not target:
+					break
+				else:
+					targets.append(target)
+
+			if targets:
+				for target in targets:
+					pf = pathfinder.PathFinder(target)
+					results += pf.paths_from(source)
+					del pf
+
+				g = pathfinder.PathFinderGraph(results, "Call graph from " + source)
+				g.Show()
+				del g
 
 def PLUGIN_ENTRY():
 	return idapathfinder_t()
